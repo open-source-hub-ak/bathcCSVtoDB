736474524dfbe25cb738ce9de005fa0bb901294d
aakash <aakash>
2021-02-17 21:04:50 +0530

    Initial Commit

---
+++ src/test/java/com/opensource/batchcsvtodb/BatchcsvtodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.batchcsvtodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchcsvtodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
