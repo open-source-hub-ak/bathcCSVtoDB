29f228e757e5ab176f8e012e2eabe2b525526326
aakash <aakash>
2021-02-17 21:58:15 +0530

    job Launcher configured in spring test

--- src/test/java/com/opensource/batchcsvtodb/BatchcsvtodbApplicationTests.java
+++ src/test/java/com/opensource/batchcsvtodb/BatchcsvtodbApplicationTests.java
@@ -1,13 +1,29 @@
 package com.opensource.batchcsvtodb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BatchcsvtodbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private JobLauncher jobLauncher;
 
+	@Autowired
+	private Job job;
+
+	@Test
+	void jobtest() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException,
+			JobParametersInvalidException {
+		jobLauncher.run(job, new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters());
+	}
 }
